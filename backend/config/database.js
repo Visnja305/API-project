85f0b0141192aa75b70c37cf48ec212524695ddd
Visnja Vracaric <visnja.vracaric@gmail.com>
2023-09-12 16:56:32 -0400

    added databse url to config/database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,7 +10,7 @@ module.exports = {
     typeValidation: true
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgres://app_academy_projects_kqt7_user:ytVVZhY9VJcUBnRGpiWzqkl8c6MuEg6D@dpg-cjn869ocfp5c73fothog-a/app_academy_projects_kqt7',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
