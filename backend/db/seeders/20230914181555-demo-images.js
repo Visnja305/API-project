4d70a45b547089ccb5280e477a4a195a2f464079
Visnja Vracaric <visnja.vracaric@gmail.com>
2023-11-08 19:21:58 -0500

    last

--- backend/db/seeders/20230914181555-demo-images.js
+++ backend/db/seeders/20230914181555-demo-images.js
@@ -10,20 +10,20 @@ module.exports = {
 
       await Image.bulkCreate([
         {
-          url:"https://tinyurl.com/2a9ahwru",
+          url:"https://tinyurl.com/3bpnpzpx",
           preview:true,
           imageableId:3,
           imageableType:"Review",
 
   },
         {
-          url:"https://tinyurl.com/ytcy7f5p",
+          url:"https://tinyurl.com/yc7afs2b",
           preview:true,
           imageableId:2,
           imageableType:"Spot",
         },
         {
-          url:"https://tinyurl.com/2s4e4ubp",
+          url:"https://tinyurl.com/mr34cay2",
           preview:true,
           imageableId:1,
           imageableType:"Spot",
